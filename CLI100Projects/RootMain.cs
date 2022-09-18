e083515d72dabe46de9741d35574e0a3be916653
mario <mario@DESKTOP-UGRV3PI>
2022-09-18 22:39:07 +0200

    Finished 17 Projects

--- CLI100Projects/RootMain.cs
+++ CLI100Projects/RootMain.cs
@@ -14,45 +14,8 @@ namespace CLI100Projects
     {
         public static void Main(string[] args)
         {
-            // Kenny
-            WL("This will be its own line");
-            W("Row");
-            W("Row");
-            W("Row");
-            W("Row");
-
-            List<string> list = new List<string>() { "1", "2", "5", "7", "8", "10" };
-            List<int> listint = new List<int>() { 1, 2, 5, 7, 8, 10 };
-
-            Sleep(1000);
-
-            ListW(list); // throws every element from list in a line with default spacer " "
-            ListW(list, spacer: "--"); // throws every element from list in a line + custom spacer --
-            // => ArrayW, ArrayWL, ListW, ListWR <=
-
-
-
-            //Vanilla C# equivalent
-            Console.WriteLine("This will be its own line");
-            Console.Write("Row");
-            Console.Write("Row");
-            Console.Write("Row");
-            Console.Write("Row");
-
-            List<string> list_ = new List<string>() { "1", "2", "5", "7", "8", "10" };
-            List<int> listint_ = new List<int>() { 1, 2, 5, 7, 8, 10 };
-
-            System.Threading.Thread.Sleep(1000);
-
-            foreach (string item in list_)
-            {
-                Console.WriteLine(item + " ");
-            }
-
-            foreach (int item in listint_)
-            {
-                Console.WriteLine(item + "--");
-            }
+            // Call the project from here with Projects.[name]... Example: =>
+            Projects.SortingAlgorithm.Sort(1900);
         }
     }
 }
