b44687becf98a54527f54e365ba95f1a48985703
mario <mario@DESKTOP-UGRV3PI>
2022-09-18 13:08:31 +0200

    Kenny Addition

--- CLI100Projects/Kenny/Set.cs
+++ CLI100Projects/Kenny/Set.cs
@@ -6,5 +6,21 @@ using System.Threading.Tasks;
 
 namespace Kenny
 {
-    public static class Set {   }
+    public static class Set
+    {
+        public static void _UnderlineForeach(string message, string spacing = "")
+        {
+            Console.WriteLine(" " + message + " ");
+            Console.Write(spacing);
+            Console.Write("|");
+            for (int i = 0; i < message.Length; i++)
+            {
+                Console.Write("-");
+            }
+            Console.Write("|\n");
+        }
+
+        public static async Task _WriteToFile(string path, string[] data) =>
+            await File.WriteAllLinesAsync(path, data);
+    }
 }
--- CLI100Projects/RootMain.cs
+++ CLI100Projects/RootMain.cs
@@ -8,15 +8,51 @@ global using System.Numerics;
 
 namespace CLI100Projects
 {
+    using static Kenny.Utilities;
+
     public class CLI100Projects
     {
         public static void Main(string[] args)
         {
-            // Every interaction with possible CLI output can be called with Projects.[Name]...
+            // Kenny
+            WL("This will be its own line");
+            W("Row");
+            W("Row");
+            W("Row");
+            W("Row");
+
+            List<string> list = new List<string>() { "1", "2", "5", "7", "8", "10" };
+            List<int> listint = new List<int>() { 1, 2, 5, 7, 8, 10 };
+
+            Sleep(1000);
+
+            ListW(list); // throws every element from list in a line with default spacer " "
+            ListW(list, spacer: "--"); // throws every element from list in a line + custom spacer --
+            // => ArrayW, ArrayWL, ListW, ListWR <=
 
 
 
-            
+            //Vanilla C# equivalent
+            Console.WriteLine("This will be its own line");
+            Console.Write("Row");
+            Console.Write("Row");
+            Console.Write("Row");
+            Console.Write("Row");
+
+            List<string> list_ = new List<string>() { "1", "2", "5", "7", "8", "10" };
+            List<int> listint_ = new List<int>() { 1, 2, 5, 7, 8, 10 };
+
+            System.Threading.Thread.Sleep(1000);
+
+            foreach (string item in list_)
+            {
+                Console.WriteLine(item + " ");
+            }
+
+            foreach (int item in listint_)
+            {
+                Console.WriteLine(item + "--");
+            }
         }
     }
 }
